9414cd550342e709ac29168ea8bbdca6458c3efe
Renato Mesquita <78408578+srmesquita22@users.noreply.github.com>
2023-06-10 16:38:36 -0300

    Adicionei arquivos via upload.

---
+++ aula/Hello.java
@@ -0,0 +1,7 @@
+package aula;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Olá turma, que bom que conheceram o Eclípse!");
+	}
+}
---
+++ aula/MinhaClasse.java
@@ -0,0 +1,17 @@
+package aula;
+
+public class MinhaClasse {
+	public static void main (String[] args) {
+		String primeiroNome = "Renato";
+		String segundoNome = "Mesquita";
+		
+		String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+		System.out.println(nomeCompleto);
+	}
+	
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+	return primeiroNome.concat(" ").concat(segundoNome);
+}
+
+}
+
